e66155433e7e8160e3c532a1f2d5074aa4f99356
rishabhshyamjoshi <59647454+rishabhshyamjoshi@users.noreply.github.com>
2020-05-21 15:04:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,10 @@ var position;
 function setup(){
     database=firebase.database();
     createCanvas(500,500);
-    square = createSprite(200,200,40,40);
+    square = createSprite(250,250,0.011,500);
+    square.shapeColor = "yellow";
+    square1 = createSprite(250,250,0.01,500);
+    square1.shapeColor = "yellow";
     hypnoticball = createSprite(250,250,10,10);
     hypnoticball.shapeColor = "red";
     hypnotic1ball = createSprite(250,250,10,10);
@@ -22,13 +25,21 @@ function draw(){
     hypnotic1ball.x = mouseX;
     hypnotic1ball.y = mouseY;
     hypnoticball.y = mouseY;
-    if(mouseX < 200){
+    if(mouseX < 255){
         hypnoticball.shapeColor = "blue";
         hypnotic1ball.shapeColor = "blue";
     }
-    else{
+    if(mouseY < 255){
         hypnoticball.shapeColor = "red";
         hypnotic1ball.shapeColor = "red";
+    }
+    if(mouseY > 255){
+        hypnoticball.shapeColor = "green";
+        hypnotic1ball.shapeColor = "green";
+    }
+    if(mouseX > 255){
+        hypnoticball.shapeColor = "yellow";
+        hypnotic1ball.shapeColor = "yellow";
     }
     drawSprites();
 }
@@ -38,7 +49,13 @@ function readPosition(data){
 position=data.val();
 hypnoticball.x=position.x;
 hypnoticball.y=position.y;
-ball1.x = position.x;
+}
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
+}
 ball1.y = position.y;
 }
 function writePosition(x,y){
